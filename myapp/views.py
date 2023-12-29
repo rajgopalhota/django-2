7df34e4641248e36cd9cb94a9f18dc7dc08d2ddc
rajgopalhota <rajgopalhota@gmail.com>
2023-12-29 10:47:48 +0530

    Todo setup for django project using npm

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
+# myapp/views.py
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, this is the home view!")
+
+def about(request):
+    return render(request, 'myapp/about.html')
